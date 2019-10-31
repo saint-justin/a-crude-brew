8396d52bda93c5a21befeaaf8794d40aff193d76
Kevin Kulp (RIT Student) <kmk8334@ad.rit.edu>
2019-10-31 13:52:44 -0400

    Created the framework for match animations, accidentally worked in Justin's scene again oop

---
+++ A Crude Brew/Assets/Scripts/DestroyAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAnimation : MonoBehaviour
+{
+    public Sprite[] spriteSheet;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        gameObject.GetComponent<SpriteRenderer>().sprite = spriteSheet[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = spriteSheet[index];
+        index++;
+        if(index >= spriteSheet.Length)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
